81bf4f7d1ed329e75e4dd7fe3c9a193c54aeab47
Damiano Braga <dbraga@mbp-3209.local>
2012-10-29 13:34:11 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dalmata::Application.config.secret_token = 'cba8d929918a067b4f18b474e11cb40505dd459f6359f936a472f0d4fd0867ca44bc8c759bf2f4ec4ceeda55a9f2ccb317cfcdd3bbf00f73de6e5a250ec0b92e'
