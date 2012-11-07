bed2f3b75c066a69b298fe2e1d29c2fa2af0e93a
Damiano Braga <dbraga@mbp-3209.local>
2012-11-07 12:13:01 -0800

    First working version of dynamic stuff

--- app/models/map.rb
+++ app/models/map.rb
@@ -11,11 +11,7 @@ class Map < ActiveRecord::Base
 def self.otherTopics(query, rows, title, snippet)
   result = json(query,rows,title, snippet)
   lastCluster = result["clusters"].last
-  if lastCluster.first.second.eql?(["Other Topics"])
-    result["clusters"].last["docs"]
-  else
-    []
-  end
+  result['clusters'].last["docsCount"]
 end
 
 def self.json(query, rows, title, snippet)
@@ -32,29 +28,64 @@ def self.json(query, rows, title, snippet)
    return result
 end
 
+def self.reverseCoordinate(c)
+  "("+c.delete("(").delete(")").delete(" ").split(",")[1] +","+ c.delete("(").delete(")").delete(" ").split(",")[0]+ ")"
+end
+
 def self.results(query = "city_s:%22SAN%20FRANCISCO%22", rows = "100", title = "geohash_s", snippet="neighborhoodDisplay_s")
   result = json(query,rows,title, snippet)
   clusters = result["clusters"]
   solrDocs = result["response"]["docs"]
   # Setting the map view
-  output = "map.setView(new L.LatLng("+solrDocs.first["latitude_f"].to_s+","+solrDocs.first["longitude_f"].to_s+"), 12);
-  var baseLayer = new L.TileLayer(truliaUrl, { attribution: truliaAttribution});
-  baseLayer.addTo(map);
-  var markers = new L.MarkerClusterGroup({ maxClusterRadius: 200 });" + "\n"
+  output = "
+    var initialZoomLevel = 9;
+
+    map.setView(new L.LatLng("+solrDocs.first["latitude_f"].to_s+","+solrDocs.first["longitude_f"].to_s+"), initialZoomLevel);
+    var baseLayer = new L.TileLayer(truliaUrl, { attribution: truliaAttribution});
+    baseLayer.addTo(map);"
+    
+       output +="map.on('zoomstart', function (a) {
+                   console.log(map.getZoom());
+                });\n"  
   # output clusters
+  
   clusters.each do |cluster|
-     output = "\n" + output +  "// Cluster: "+cluster["labels"].first + "\n"
-     cluster["docs"].each do |doc|
-        solrDocs.each do |solrDoc|
-           if solrDoc["id"].eql?(doc) && (!solrDoc["latitude_f"].to_s.blank?) && (!solrDoc["longitude_f"].to_s.blank?)
-            output = "\n" + output + 
-            "var marker = new L.Marker(new L.LatLng("+solrDoc["latitude_f"].to_s + " , " + solrDoc["longitude_f"].to_s+"), { title: '"+cluster["labels"].first.delete("'")+"'}); 
-            marker.bindPopup('"+cluster["labels"].first.delete("'")+"');
-            markers.addLayer(marker);\n"
-           end       
-        end
-     end
+    # cluster = clusters.first
+    clusterName = cluster["labels"].first.delete("'")
+    if !clusterName.eql?("Other Topics")
+      clusterCentroid = reverseCoordinate(cluster["docsCentroid"])
+      docsPoints = cluster["docsPoints"]
+      output += "// Cluster: #{clusterName} \n"
+      output += "// Count: #{cluster["docsCount"]} \n"
+      output += "// Docs Centroid: #{cluster["docsCentroid"]} \n"
+      output += "// Range Price: #{cluster["rangePrice"]} \n"
+      # output +="var markers = new L.MarkerClusterGroup({ maxClusterRadius: 80 });\n"
+      # output +="var markers = new L.MarkerClusterGroup();\n"
+      # output +="var markers = new L.MarkerClusterGroup({ options: {
+      #     iconCreateFunction: 
+      #       function(cluster) {
+      #         return new L.DivIcon({ html: '<b></b>' });
+      #       }
+      #     }
+      #   });\n"
+
+      output +="var markers = new L.MarkerClusterGroup({title: 'test'});"
+      docsPoints.each do |d|
+        reversePoint = reverseCoordinate(d)
+        output += "var marker = new L.Marker(new L.LatLng#{reversePoint}, { title: '#{clusterName}'}); 
+        marker.bindPopup('#{clusterName}');
+        markers.addLayer(marker);\n"
+      end
+       output += "map.addLayer(markers);\n"
+    end
+   
+
   end
-  output + "map.addLayer(markers);"
+
+    
+
+
+  
+  output
   end
 end
